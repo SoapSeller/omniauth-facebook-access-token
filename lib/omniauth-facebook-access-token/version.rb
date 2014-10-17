11f2830f34b7ed07d8cd663acb87d021a3aa9478
Dor Shahaf <dor@shahaf.com>
2014-10-17 17:44:54 +0300

    Remove setting of SSL version + bump version to 0.1.6.

--- lib/omniauth-facebook-access-token/version.rb
+++ lib/omniauth-facebook-access-token/version.rb
@@ -1,5 +1,5 @@
 module OmniAuth
   module FacebookAccessToken
-    VERSION = '0.1.5'
+    VERSION = '0.1.6'
   end
 end
--- lib/omniauth/strategies/facebook-access-token.rb
+++ lib/omniauth/strategies/facebook-access-token.rb
@@ -15,8 +15,7 @@ module OmniAuth
 
       option :client_options, {
         :site => 'https://graph.facebook.com',
-        :token_url => '/oauth/access_token',
-        :ssl => { :version => "SSLv23" }
+        :token_url => '/oauth/access_token'
       }
 
       option :access_token_options, {
